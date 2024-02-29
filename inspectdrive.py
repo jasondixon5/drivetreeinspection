30d08ce088de8ac7c6747c748182f1432e7e420b
Jason Dixon <jasondixonmail@gmail.com>
2024-02-29 14:30:35 -0800

    MVP created that fetches some info on each file and writes to csv

---
+++ inspectdrive.py
@@ -0,0 +1,147 @@
+import csv
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
+
+# TODOS:
+# * Set output to screen for each request so know script is still working
+# * Look into read operation timeout error; possible need for chunking
+# * Add path info (key?)
+# * Avoid writing headers if not first write to file
+
+
+
+def main():
+  """Shows basic usage of the Drive v3 API.
+  Prints the names and ids of the first 10 files the user has access to.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  request_file_info(creds)
+  
+  # try:
+    
+
+  #   # Call the Drive v3 API
+  #   results = (
+  #       service.files()
+  #       .list(
+  #           pageSize=100, 
+  #           fields="nextPageToken, files(id, name)",
+  #           q="trashed=false")
+  #       .execute()
+  #   )
+  #   items = results.get("files", [])
+
+  #   handle_items(items)
+
+  #   page_token = results.get("nextPageToken")
+
+  #   print("Next page token", page_token)
+    
+  #   if page_token:
+      
+  #     results = (
+  #           service.files()
+  #           .list(
+  #               pageSize=100, 
+  #               fields="nextPageToken, files(id, name)",
+  #               q="trashed=false",
+  #               pageToken=page_token,)
+  #           .execute()
+  #     )
+  #     # print(results, '\n')
+
+  #     items = results.get("files", [])
+
+  #     handle_items(items)
+
+  
+  # except HttpError as error:
+  #   # TODO(developer) - Handle errors from drive API.
+  #   print(f"An error occurred: {error}")
+
+
+def handle_items(items):
+
+  if not items:
+    print("No files found.")
+    return
+  # print("Files:")
+  try:
+    
+    # print(items)
+    with open('inspect_results.csv', 'a', newline='') as csvfile:
+        fieldnames = ['id', 'name']
+        writer=csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for item in items:
+          writer.writerow({
+            'id': item['id'], 
+            'name': item['name'],
+            })
+        # writer.writerows(items)
+        # writer.writerows([f"{item['name']}", f"({item['id']})"])
+        # writer.writerows(items)
+  
+  except Exception as e:
+      print(f"Exception encountered when trying to write to file:\n\n{e}")
+
+def request_file_info(creds):
+  call_count = 0
+
+  try:
+    service = build("drive", "v3", credentials=creds)
+  
+    while call_count >= 0:
+      results = (
+        service.files()
+        .list(
+            pageSize=100, 
+            fields="nextPageToken, files(id, name)",
+            q="trashed=false")
+        .execute()
+      )
+      
+      page_token = results.get("nextPageToken")
+      items = results.get("files", [])
+      handle_items(items)
+
+      if page_token:
+        call_count += 1
+      else:
+        call_count = -1
+  
+  except HttpError as error:
+    # TODO(developer) - Handle errors from drive API.
+    print(f"An error occurred: {error}")
+
+  print("Finished drive inspection script.")
+
+ 
+if __name__ == "__main__":
+  main()
